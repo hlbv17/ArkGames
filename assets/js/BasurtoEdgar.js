72c1525d8539c20a1fb9aef24ae646b3df38b60e
edgarbasurto <edgar.basurtoc@ug.edu.ec>
2022-01-16 10:52:05 -0500

    paginado

--- assets/js/BasurtoEdgar.js
+++ assets/js/BasurtoEdgar.js
@@ -1,26 +1,29 @@
-var pagActual = 1;
-var selectedOption = document.getElementById('cantreg').value;
+var paginas = document.getElementById('numpagina');
 var cuadriculas = document.querySelectorAll('.cuadricula');
-var totalPaginas = cuadriculas.length / selectedOption;
-
 var select = document.getElementById('cantreg');
+var selectedOption = select.options[select.selectedIndex].text;
+var selectedPage = paginas.options[paginas.selectedIndex].text;
+
 select.addEventListener('change',
     function () {
         selectedOption = this.options[select.selectedIndex].text;
-        actualizar();
-        console.log(totalPaginas);
     });
 
-function actualizar() {
-    for (let index = 0; index < cuadriculas.length; index++) {
-        const element = cuadriculas[index];
-        if (element.id != 1 & element.id != 2 & element.id != 3) {
-            element.style.display = "none";
+paginas.addEventListener('change',
+    function () {
+        selectedPage = this.options[paginas.selectedIndex].text;
+        console.log(selectedPage*selectedOption);
+        console.log((selectedPage*selectedOption) - selectedOption);
+
+        for (let index = 0; index < cuadriculas.length; index++) {
+            var element = cuadriculas[index];
+            if (element.id <= (selectedPage*selectedOption) && element.id > ((selectedPage*selectedOption) - selectedOption) ) {
+                element.style.display = "block";
+            } else {
+                element.style.display = "none";
+            }
         }
+    });
 
-    }
-}
-
-actualizar();
-console.log(totalPaginas);
+    
 
