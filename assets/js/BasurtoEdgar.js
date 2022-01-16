f6320406928d80248c8be6280cd65425e3984f6a
edgarbasurto <edgar.basurtoc@ug.edu.ec>
2022-01-16 08:16:59 -0500

    paginado

--- assets/js/BasurtoEdgar.js
+++ assets/js/BasurtoEdgar.js
@@ -1,14 +1,26 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const pagActual = 1;
+var pagActual = 1;
+var selectedOption = document.getElementById('cantreg').value;
+var cuadriculas = document.querySelectorAll('.cuadricula');
+var totalPaginas = cuadriculas.length / selectedOption;
 
-    let cuadriculas = document.querySelectorAll('.cuadricula');
+var select = document.getElementById('cantreg');
+select.addEventListener('change',
+    function () {
+        selectedOption = this.options[select.selectedIndex].text;
+        actualizar();
+        console.log(totalPaginas);
+    });
 
+function actualizar() {
     for (let index = 0; index < cuadriculas.length; index++) {
         const element = cuadriculas[index];
-        if (element.id != 1 & element.id !=2 & element.id !=3) {
-            element.style.display="none";
+        if (element.id != 1 & element.id != 2 & element.id != 3) {
+            element.style.display = "none";
         }
-       
-    }
 
-})
+    }
+}
+
+actualizar();
+console.log(totalPaginas);
+
